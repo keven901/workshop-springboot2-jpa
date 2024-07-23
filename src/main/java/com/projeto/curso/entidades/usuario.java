61961ce4c8a3cc0f8463ddf2f00caab6994e37d6
keven901 <kevenmarques901@gmail.com>
2024-07-23 17:27:47 -0300

    H2 database, test profile, JPA

--- src/main/java/com/projeto/curso/entidades/usuario.java
+++ src/main/java/com/projeto/curso/entidades/usuario.java
@@ -3,10 +3,17 @@ package com.projeto.curso.entidades;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class usuario implements Serializable {
 	
-	
 	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
